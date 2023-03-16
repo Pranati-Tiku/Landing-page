60645290a2a30ed26cd83190bda54510d5f953ff
Pranati Tikku <pranatitikku@gmail.com>
2023-03-16 21:39:31 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    container:{
+      center:true,
+      screens:{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px",
+            },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
